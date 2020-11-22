e2063e947683ab2e2454207db109f1f7927e730f
Dong Kyu Sung <dongkyusung@Dongs-MacBook-Pro.local>
2020-11-22 15:15:53 +0900

    getting data from client to server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var Promise = require("bluebird");
-// var sqlite = require('sqlite');
-// import { open } from 'sqlite'
+
+
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./total_1G.db');
 
@@ -9,15 +9,6 @@ let db = new sqlite3.Database('./total_1G.db');
 
 const app = express();
 const port = 4000;
-// const dbPromise = sqlite.open('/total_1G.db', { Promise });
-// async function openDB () {
-//   return sqlite.open({
-//     filename: '/total_1G.db',
-//     driver: sqlite.Database
-//   })
-// }
-
-// const db = openDB();
 
 
 app.set('views', __dirname + '/');
@@ -32,30 +23,8 @@ app.listen(port, () => {
   console.log("listening on port " + port);
 });
 
-// let sql = 'SELECT progName, startTime FROM More1GApp where progName == "progcorhbov.e" group by jobID';
-// db.all(sql, [], (err, rows) => {
-//   if (err) {
-//     throw err;
-//   }
-//   rows.forEach((row) => {
-//     console.log(row.name);
-//   });
-// });
-
-// app.get('/getData', async (req, res, next) => {
-//   console.log("get data backend");
-//   try {
-//     const db = await openDB();
-//     const appinfo = await db.all('SELECT progName, startTime FROM More1GApp where progName == "progcorhbov.e" group by jobID');
-//     console.log(JSON.stringify(appinfo))
-//     res.json(appinfo)
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
 app.get('/getData', async (req, res, next) => {
-  let sql = 'SELECT progName, numOST, writeRateTotal, writeBytesTotal FROM More1GApp where progName == "pw.x" AND numOST < 20 group by jobID';
+  let sql = 'SELECT progName, numOST, writeRateTotal, writeBytesTotal FROM More1GApp where progName == "'+req.query.progName+'" AND numOST < 20 group by jobID';
   db.all(sql, [], (err, rows) => {
     if (err) {
       throw err;
@@ -64,6 +33,7 @@ app.get('/getData', async (req, res, next) => {
       console.log(row.progName);
     });
     console.log(rows.length)
+    console.log(req.query)
     res.json(rows);
   });
 });
