25c3abec188388c6420aa43a39e1f1bb81f9232e
Dong Kyu Sung <dongkyusung@Dongs-MacBook-Pro.local>
2020-12-03 15:24:17 +0900

    lastest change

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var Promise = require("bluebird");
 
 const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./total_1G.db');
+let dbOrig = new sqlite3.Database('./total_all.db')
 
 
 
@@ -13,6 +14,8 @@ const port = 4000;
 
 app.set('views', __dirname + '/');
 app.engine('html', require('ejs').renderFile);
+
+app.use("/static", express.static('./static/'));
 
 
 app.get('/', (req, res, next) => {
@@ -39,15 +42,146 @@ app.get('/getData', async (req, res, next) => {
 });
 
 app.get('/appendOptions', async (req, res, next) => {
-  let sql = "SELECT progName, COUNT(*) AS 'num' FROM More1GApp GROUP BY progName HAVING COUNT(*) > 80 ORDER BY COUNT(*) DESC;";
+  console.log("getting data for app names for options")
+  let sql = "SELECT progName, COUNT(*) AS 'count' FROM More1GApp GROUP BY progName HAVING COUNT(*) > 80 ORDER BY COUNT(*) DESC;";
+  db.all(sql, [], (err, rows) => {
+    if (err) { throw err; }
+    rows.forEach((row) => {
+      if (row.progName === 'cp.eba9534c022ba7e1e8e7c4bc0d60ea8eeab197b6_gnu_tg_libxc.x') row.optionName = 'cp.eba_libxc';
+      else if (row.progName === 'cp.8d07d5422a2a994f120958502fbb3b771a6bf7b7_gnu.x') row.optionName = 'cp.8d07_gnu.x';
+      else if (row.progName === 'cp.x_cpno_cpp_part-163-gbef2c14-constrained_dynamics-cbya.2017-11-14') row.optionName = 'cp.x_163';
+      else if (row.progName === 'cp.x_no_cpp_part-153-g69a9a1c.flexible_ref_cell.2017-11-30') row.optionName = 'cp.x_153';
+      else row.optionName = row.progName;
+    });
+    res.json(rows);
+  });
+});
+
+app.get('/getMDS', async (req, res, next) => {
+  console.log("getting MDS information for selected jobID " + req.query.jobID)
+  let sql = "SELECT startTime, endTime FROM More1GApp WHERE jobID == '"+req.query.jobID+"' GROUP BY jobID";
+
+  db.all(sql, [], (err, rows) => {
+    let st, et;
+    // let stDate, etDate;
+    if (err) { throw err; }
+    rows.forEach((row) => {
+      // console.log("startTime: "+row.startTime+" endTime: "+row.endTime);
+      st = row.startTime;
+      et = row.endTime;
+      // stDate = new Date(st);
+      // etDate = new Date(et);
+    });
+
+    // do another thing
+    // sql = "SELECT mdsCPU95, mdsOPS95, startTime, endTime FROM More1GApp WHERE (startTime <= '" + st + "' AND endTime >= '" + et + "') OR (startTime >= '" + st + "' AND endTime <= '" + et + "') group by jobID;";
+    sql = "SELECT mdsCPU95, mdsOPS95, startTime, endTime FROM total WHERE (startTime <= '" + st + "' AND endTime >= '" + et + "') OR (startTime >= '" + st + "' AND endTime <= '" + et + "') group by jobID;";
+    // db.all(sql, [], (err, rows) => {
+    dbOrig.all(sql, [], (err, rows) => {
+        if (err) { throw err; }
+      let result = [];
+      rows.forEach((row) => {
+        // console.log("mdsCPU: "+row.mdsCPU95+" mdsOPS: "+row.mdsOPS95);
+        let newST = new Date(row.startTime);
+        let newET = new Date(row.endTime);
+        // console.log("start: "+ st + " Endtim: "+et)
+        let md = new Date(newET - (newET-newST)/2);
+        result.push({/*origST: stDate.getTime(), origET: etDate.getTime(),*/ time: md.getTime(), cpu: row.mdsCPU95, ops: row.mdsOPS95});
+        //solve this time problem
+        // console.log(result);
+        result.sort(function(x, y){
+          return x.time - y.time;
+        });
+      });
+      res.json(result);
+    });
+  });
+});
+
+//hrchung
+app.get('/getIntervalJobs', async (req, res, next) => {
+  // TODO : change the range of filtering 
+  let sql = 'SELECT progName, jobID, startTime, endTime, writeBytesTotal, numOST, ostlist FROM More1GApp where startTime <= \
+  (SELECT startTime FROM More1GApp where  progName == "' + req.query.progName+'" AND jobID=='+req.query.jobID +') \
+  AND endTime >= (SELECT startTime FROM More1GApp where  progName == "' + req.query.progName+'" AND jobID=='+req.query.jobID+')';
+
   db.all(sql, [], (err, rows) => {
     if (err) {
       throw err;
     }
-    rows.forEach((row) => {
-      console.log(row.progName);
-    });
-    console.log(rows.length)
+    //console.log("[getIntervalJobs] "+rows.length)
     res.json(rows);
   });
 });
+
+app.get('/getAppInfo', async (req, res, next) => {
+  console.log("going to get app info of "+req.query.progName);
+  let sql = 'SELECT progName, jobID, startTime, endTime, runTime, numOST, numProc, totalFile, totalWriteReq FROM More1GApp where progName == "'+req.query.progName+'" group by jobID';
+  db.all(sql, [], (err, rows) => {
+    if (err) { throw err; }
+    rows.forEach((row) => {
+      row.startTime = changeYear(row.startTime);
+      row.endTime = changeYear(row.endTime);
+
+      if (row.progName === 'cp.eba9534c022ba7e1e8e7c4bc0d60ea8eeab197b6_gnu_tg_libxc.x') row.optionName = 'cp.eba_libxc';
+      else if (row.progName === 'cp.8d07d5422a2a994f120958502fbb3b771a6bf7b7_gnu.x') row.optionName = 'cp.8d07_gnu.x';
+      else if (row.progName === 'cp.x_cpno_cpp_part-163-gbef2c14-constrained_dynamics-cbya.2017-11-14') row.optionName = 'cp.x_163';
+      else if (row.progName === 'cp.x_no_cpp_part-153-g69a9a1c.flexible_ref_cell.2017-11-30') row.optionName = 'cp.x_153';
+      else row.optionName = row.progName;
+    });
+    res.json(rows);
+  });
+});
+
+app.get('/getOST', async (req, res, next) => {
+  // TODO : change the range of filtering 
+  let sql = 'SELECT progName, jobID, startTime, endTime, writeBytesTotal, numOST, ostlist FROM More1GApp where \
+  (startTime <= (SELECT startTime FROM More1GApp where jobID=='+req.query.jobID +') \
+  AND endTime >= (SELECT startTime FROM More1GApp where jobID=='+req.query.jobID+')) \
+  OR (startTime >= (SELECT startTime FROM More1GApp where jobID=='+req.query.jobID +') \
+  AND endTime <= (SELECT startTime FROM More1GApp where jobID=='+req.query.jobID+')) group by jobID';
+
+  // let sql = 'SELECT progName, jobID, startTime, endTime, writeBytesTotal, numOST, ostlist FROM More1GApp where startTime <= \
+  // (SELECT startTime FROM More1GApp where jobID=='+req.query.jobID +') \
+  // AND endTime >= (SELECT startTime FROM More1GApp where jobID=='+req.query.jobID+')';
+
+
+  db.all(sql, [], (err, rows) => {
+    if (err) { throw err; }
+    res.json(rows);
+  });
+});
+
+app.get('/getHT', async (req, res, next) => {
+  // TODO : change the range of filtering 
+  let sql = 'SELECT progName, jobID, startTime, endTime, writeBytesTotal, numOST, ostlist FROM More1GApp where \
+  (startTime <= (SELECT startTime FROM More1GApp where jobID=='+req.query.jobID +') \
+  AND endTime >= (SELECT startTime FROM More1GApp where jobID=='+req.query.jobID+')) \
+  OR (startTime >= (SELECT startTime FROM More1GApp where jobID=='+req.query.jobID +') \
+  AND endTime <= (SELECT startTime FROM More1GApp where jobID=='+req.query.jobID+')) group by jobID';
+
+  db.all(sql, [], (err, rows) => {
+    if (err) { throw err; }
+    res.json(rows);
+  });
+});
+
+function changeYear(st) {
+  st = String(st);
+  let st1, st2, newChar1, newChar2, newst;
+
+  if (st.charCodeAt(3) <= 55) {
+    newChar1 = String.fromCharCode(57);
+    st1 = st.substring(0,3);
+    st2 = st.substring(4);
+    newst = st1 + newChar1 + st2;
+  } else if (st.charCodeAt(3) >= 56) {
+    newChar1 = String.fromCharCode(st.charCodeAt(2)+1);
+    newChar2 = String.fromCharCode(48);
+    st1 = st.substring(0,2);
+    st2 = st.substring(4);
+    newst = st1 + newChar1 + newChar2 + st2;
+  }
+
+  return newst;
+}
---
+++ static/comm.js
@@ -0,0 +1,416 @@
+
+$(function() { //functions to be called on load of html
+  appendOptions();
+
+  d3.select('#select1').on('change', function (event, k) { // ...
+    // console.log("call select update")
+    let progName = $("#select1 option:checked").val();
+    // console.log(progName);
+    getAppInfo(progName);
+
+  });
+
+});
+
+
+function appendOptions() {
+  console.log("going to append option to select1");
+  $.getJSON("/appendOptions", function(json) {
+    json.forEach((row) => {
+      d3.select("#select1")
+        .append("option")
+        .property('value', row.progName)
+        .attr('class', 'appOption')
+        .attr('id', row.progName)
+        .text(row.optionName);
+    });
+  });
+}
+
+function getData() {
+    console.log("going to be db data")
+    let progName = {progName: "pw.x"}
+  $.getJSON("/getData", progName, function (json) {
+      console.log("hi");
+      console.log(json);
+  });
+}
+
+
+function getMDSData(jid) {
+  console.log("going to be MDS data")
+  let jobID = {jobID: jid}
+  $.getJSON("/getMDS", jobID, function (data) {
+      // console.log(data);
+      let mdsLines = ["OPS", "CPU"];
+      let mdsInfo = ["MDS # of Operations", "MDS CPU Usage"];
+
+      MDSxAxis = d3.scaleTime().domain([d3.min(data, d => d.time), d3.max(data, d => d.time)])
+          .range([70, 1120]); //1050 width
+      //ops
+      MDSyAxisL = d3.scaleLinear().domain([d3.min(data, d => d.ops), d3.max(data, d => d.ops)])
+          .range([250, 10]);
+      //cpu
+      MDSyAxisR = d3.scaleLinear().domain([0, 100])
+          .range([250, 10]);
+
+      //line function
+      lineOPS = d3.line().x(d => MDSxAxis(d.time)).y(d => MDSyAxisL(d.ops));
+      lineCPU = d3.line().x(d => MDSxAxis(d.time)).y(d => MDSyAxisR(d.cpu));
+
+      //append x axis to svg
+      MDSxAxisVar
+        .attr('transform', `translate(0, 250)`)
+        .call(d3.axisBottom(MDSxAxis));
+
+      MDSyAxisLVar
+        .attr('transform', `translate(70, 0)`)
+        .call(d3.axisLeft(MDSyAxisL));
+
+      MDSyAxisRVar
+        .attr('transform', `translate(1110, 0)`)
+        .call(d3.axisRight(MDSyAxisR));
+
+      MDSlines = MDSsvg
+                  .selectAll('path.line')
+                  .data(mdsLines, d => d)
+                  .join('path')
+                    .attr('class', d => d)
+                    .classed('line', true)
+                    .attr('d', function (d, i) { 
+                      if (d === "OPS") return lineOPS(data);
+                      else if (d === "CPU") return lineCPU(data);
+                    })
+                    .attr('fill', 'none')
+                    .attr('stroke', function(d) {
+                      if (d === "OPS") return 'green';
+                      else if (d === "CPU") return 'blue';
+                    })
+                    .attr('stroke-width', 1);
+
+      // MDSsvg.append("line")
+      //   .attr("x1", MDSxAxis(data[0].origST))  //<<== change your code here
+      //   .attr("y1", 10)
+      //   .attr("x2", MDSxAxis(data[0].origST))  //<<== and here
+      //   .attr("y2", 250)
+      //   .style("stroke-width", 1)
+      //   .style("stroke-dasharray", 4)
+      //   .style("stroke", "red")
+      //   .style("fill", "none");
+
+      // MDSsvg.append("line")
+      //   .attr("x1", MDSxAxis(data[0].origET))  //<<== change your code here
+      //   .attr("y1", 10)
+      //   .attr("x2", MDSxAxis(data[0].origET))  //<<== and here
+      //   .attr("y2", 250)
+      //   .style("stroke-width", 1)
+      //   .style("stroke", "red")
+      //   .style("fill", "none");
+
+      legendEnter = MDSsvg.select("#MDSLegend")
+        .selectAll("text")
+        .data(mdsInfo)
+        .join("text")
+          .attr("y", function(d, i) { return (i*20) + 15; })
+          .attr("x", 13)
+          .attr("font-size", '9px')
+          .attr("text-anchor", "left")
+          .style("alignment-baseline", "middle")
+          .text(d => d);
+
+      legendEnter = MDSsvg.select("#MDSLegend")
+        .selectAll("circle")
+        .data(mdsInfo)
+        .join("circle")
+          .attr("cx", 8)
+          .attr("cy", function(d,i){ return (i*20) + 15; })
+          .attr("r", 3)
+          .style("fill", function(d, i){ 
+            if(i == 0) return 'green';
+            else if (i == 1) return 'blue';
+          });
+
+      MDSYaxisLTitle = MDSsvg.select("#MDSYaxisLTitle")
+                        .append('text')
+                        .attr("text-anchor", "end")
+                        .attr("transform", "rotate(-90)")
+                        .attr("font-size", '10px')
+                        .attr("y", 10)
+                        .attr("x", -50)
+                        .text("# of MDS Operations (Per Second)");
+
+      MDSYaxisRTitle = MDSsvg.select("#MDSYaxisLTitle")
+                        .append('text')
+                        .attr("text-anchor", "end")
+                        .attr("transform", "rotate(-90)")
+                        .attr("font-size", '10px')
+                        .attr("y", 1160)
+                        .attr("x", -100)
+                        .text("CPU Usage (%)");
+
+  }); //end of getJSON
+}
+
+function getAppInfo(progName) {
+  let pn = {progName: progName}
+  $.getJSON("/getAppInfo", pn, function (json) {
+    let data = json;
+
+    appTable = d3.select("#appInfoTable");
+    appTable.selectAll('tr.appInfoContent')
+          .data(data, d => d.jobID)
+          .join('tr')
+            .classed("appInfoContent", true)
+            .attr('id', d => d.jobID)
+            .each(function(d, i) { 
+              d3.select(this).append("td").text(d => d.optionName);
+              d3.select(this).append("td").text(d => d.jobID);
+              d3.select(this).append("td").text(d => d.startTime);
+              d3.select(this).append("td").text(d => d.endTime);
+              d3.select(this).append("td").text(d => d.runTime);
+              d3.select(this).append("td").text(d => d.numOST+1);
+              d3.select(this).append("td").text(d => d.numProc);
+              d3.select(this).append("td").text(d => d.totalFile);
+              d3.select(this).append("td").text(d => d.totalWriteReq);
+            });
+    
+    console.log("app info appending for "+progName+" is done");
+
+    $("tr.appInfoContent").click(function () {
+      let jid = $(this).attr('id');
+
+      console.log("app info clicked "+$(this).attr('id'));
+
+      d3.selectAll('tr.appInfoContent').classed('tableClicked', false);
+      d3.select(this).node().classList.add("tableClicked")
+
+      let TDs = $(this).children();
+      appInfo = {progName: TDs[0].textContent, jobID: TDs[1].textContent, startTime: TDs[2].textContent, endTime: TDs[3].textContent};
+      // appInfoArr.push({progName: TDs[0].textContent, jobID: TDs[1].textContent, startTime: TDs[2].textContent, endTime: TDs[3].textContent});
+      
+      //put info into history modal
+      appendHistory(appInfo);
+      //show mds graph for the app instance
+      getMDSData(jid);
+      //show ost graph for the app instance
+      getIntervalJobs(jid);
+
+      //try
+      // makeHeatmap();
+    });
+   
+  });
+}
+
+function appendHistory(data) {
+  console.log("entering append history");
+
+  historyTable = d3.select("#historyTable");
+  historyTable = historyTable.append('tr').attr("class", "historyContent");
+
+  historyTable.append("td").html('<button type="button" class="close" id="closeButton'+data.jobID+'" aria-label="Close"><span aria-hidden="true">&times;</span></button>');
+  historyTable.append("td").text(data.progName);
+  historyTable.append("td").text(data.jobID);
+  historyTable.append("td").text(data.startTime);
+  historyTable.append("td").text(data.endTime);
+
+  $("#clearHistory").click(function () {
+    d3.selectAll('tr.historyContent').remove();
+  });
+
+  $("#closeButton"+data.jobID).click(function () {
+    d3.select(this.parentNode.parentNode).remove();
+  });
+
+}
+
+function getIntervalJobs(jobID) {
+  let q_input = {jobID : jobID}
+  $.getJSON("/getOST", q_input, function (json) {
+    var intervalJobs = json;
+    //progName, jobID, startTime, endTime, writeBytesTotal, numOST, ostlist
+    var totalOST = {};
+    var lists;
+    intervalJobs.map(row => {if(row['jobID']==q_input.jobID) { lists = row['ostlist'].split(" "); lists.map(ost => totalOST[ost] = {})} });
+    console.log(lists);
+    intervalJobs = intervalJobs.filter(item => item['numOST'] > 1);
+    intervalJobs.map(row => {var lists = row['ostlist'].split(" ");  lists.map(ost => { if (ost in totalOST) totalOST[ost][row['jobID']] = [row['writeBytesTotal']/row['numOST'], -1]; })  });
+    //barChart.totalOST = Object.getOwnPropertyNames(totalOST).map(function(e) { totalOST[e]['sum'] = Object.values(totalOST[e]).reduce((a, b) => a + b, 0); totalOST[e]['ostID']=e; return totalOST[e]});
+    // TODO : sort and select head (50);
+    barChart.totalOST = Object.getOwnPropertyNames(totalOST).map(function(e) { totalOST[e]['sum'] = Object.keys(totalOST[e]).reduce( (sum,key) => { var curr_sum = sum+totalOST[e][key][0]; totalOST[e][key][1]=curr_sum; return curr_sum},0); totalOST[e]['ostID']=e;  return totalOST[e]});
+    console.log(barChart.totalOST);
+
+    var displayOST = 10;
+    // barChart.totalOST = barChart.totalOST.sort(function(a, b) { return b['sum'] - a['sum'];}).slice(0, displayOST);
+    barChart.totalOST = barChart.totalOST.sort(function(a, b) { return b['sum'] - a['sum'];});
+
+    // barChart.totalOSTOrig = totalOST;
+    console.log(totalOST);
+    // console.log(barChart.totalOST);
+
+    // barChart.initialize();
+    barCreate(jobID, lists.length);
+ });
+};
+
+function barCreate(origJobID, lenOSTList) {
+  // console.log()
+  barChart.width = lenOSTList * 95;
+  barChart.svg.attr('width', barChart.width + 100);
+
+  barChart.ostList = Object.values(barChart.totalOST).map(d=>d['ostID']);
+  // console.log(barChart.ostlist);
+  barChart.xScale = d3.scaleBand().domain(barChart.ostList).range([50, barChart.width+50]).padding(0.05);
+  barChart.yMAX = d3.max(barChart.totalOST.map(d => {return d.sum}));
+  barChart.yScale = d3.scaleLinear().domain([0, barChart.yMAX]).range([barChart.height,10]);    
+
+  barChart.xaxis.attr('transform', "translate(0, "+ barChart.height +")").call(d3.axisBottom(barChart.xScale));
+  barChart.yaxis.attr('transform', "translate(50, 0)").call(d3.axisLeft(barChart.yScale));
+  barChart.rects = barChart.plot.selectAll('g').data(barChart.totalOST, d=>d['ostID']).join('g')
+        .attr("class", d=>d['team'])
+        .each(function(d, i) { 
+            d3.select(this).selectAll('rect').data(k => { var keys = Object.keys(k); return keys.map(eachKey => [eachKey, k[eachKey]])} , tuple => tuple[1])
+            .join('rect')
+              .attr('fill', (sub_d, sub_i) => "rgb("+Math.floor(Math.random()*256)+"," +Math.floor(Math.random()*256)+","+ Math.floor(Math.random()*256)+")")
+              .attr("width", barChart.xScale.bandwidth()-40)
+              .attr("height", (sub_d, sub_i) =>{ if(sub_d[0]!='ostID' && sub_d[0] !='sum') { return barChart.height - barChart.yScale(sub_d[1][0]);} else 0;})
+              .attr("x", margin.left+barChart.xScale(d.ostID))
+              .attr("y", (sub_d, sub_i) => {if(sub_d[0]!='ostID' && sub_d[0] !='sum') return barChart.yScale(sub_d[1][1])})
+              .attr("class", sub_d=>d.ostID)
+              //.attr("text", sub_d => {if(sub_d[0]!='ostID' && sub_d[0] !='sum') return sub_d[0]+"/"+sub_d[1]})
+              //.on('mouseover', (event, sub_d) => highlight(d.ostID))
+              //.on('mouseout', (event, sub_d) => unhighlight(d.ostID))
+        });  
+}
+
+function makeHeatmap(jobID) {
+  let jid = {jobID : jobID}
+  $.getJSON("/getHT", jid, function (json) { 
+    var intervalJobs = json;
+    //progName, jobID, startTime, endTime, writeBytesTotal, numOST, ostlist
+    let totalOST = new Array();
+    let ostList = {};
+    let onlyOST = {};
+    // var lists;
+    intervalJobs.map(row => { 
+      var lists = row['ostlist'].split(" "); 
+      if (row['jobID']==jid.jobID) {
+        lists.map(ost => {
+          if(ost in ostList) {
+
+          } else {
+            totalOST.push({ostID: parseInt(ost)});
+            ostList[ost] = ost;
+          }
+        });
+        // lists.map(ost => onlyOST.push({ostID: ost}));
+      }
+      // lists.map(ost => totalOST.push({ostID: ost}));
+      
+    });
+    console.log(ostList);
+    console.log(totalOST);
+
+    intervalJobs = intervalJobs.filter(item => item['numOST'] > 1);
+    totalOST.map(row => { row.size = 0; });
+    intervalJobs.map(row => {
+      var lists = row['ostlist'].split(" ");  
+      lists.map(ost => { 
+        totalOST.map(d => {
+          if(ost in ostList) {
+            if(d.ostID == parseInt(ost)) {
+              d.size = d.size + (row.writeBytesTotal / (row.numOST + 1));
+            }
+          }   
+        });
+      });
+    });
+
+    console.log(totalOST);
+
+    let data = new Array();
+
+    totalOST.map(row => {
+      let xIndex = Math.floor((row.ostID / 15) * 15);
+      let yIndex = row.ostID % 15;
+      data.push({ostID: row.ostID, xIndex: xIndex, yIndex: yIndex, size: row.size});
+    });
+    
+    console.log(data);
+
+
+    // myGroups = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+    myGroups = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
+    // myVars = [0, 15, 30, 45, 60, 75, 90 ,105, 120, 135, 150, 165, 180, 195, 210, 225, 240];
+    myVars = ["0", "15", "30", "45", "60", "75", "90" ,"105", "120", "135", "150", "165", "180", "195", "210", '225', "240"];
+
+    HTxAxis = d3.scaleBand()
+      .range([ 30, heatMap.width + 30])
+      .domain(myGroups)
+      .padding(0.01);
+
+    HTyAxis = d3.scaleBand()
+      .range([ 0, heatMap.height ])
+      .domain(myVars)
+      .padding(0.01);
+
+    HTxAxisVar
+      .attr("transform", "translate(0," + heatMap.height + ")")
+      .call(d3.axisBottom(HTxAxis));
+
+    HTyAxisVar
+      // .attr("transform", "translate(0," + heatMap.height + ")")
+      .attr("transform", "translate(30,0)")
+      .call(d3.axisLeft(HTyAxis));
+
+
+    let myColor = d3.scaleLinear()
+            // .range(["white", "#69b3a2"])
+            .range(["#fff5f0", "#67000d"])
+            .domain([0,d3.max(data, d => d.size)]);
+
+    // data = [
+    //   {group: "A", variable: "v1", value: 30},
+    //   {group: "A", variable: "v2", value: 95},
+    //   {group: "A", variable: "v3", value: 22},
+    //   {group: "A", variable: "v4", value: 14},
+    //   {group: "A", variable: "v5", value: 52},
+    //   {group: "A", variable: "v6", value: 30},
+    //   {group: "A", variable: "v7", value: 20},
+    //   {group: "A", variable: "v8", value: 99},
+    //   {group: "A", variable: "v9", value: 52},
+    //   {group: "A", variable: "v10", value: 30},
+    //   {group: "B", variable: "v1", value: 30},
+    //   {group: "B", variable: "v2", value: 95},
+    //   {group: "B", variable: "v3", value: 22},
+    //   {group: "B", variable: "v4", value: 14},
+    //   {group: "B", variable: "v5", value: 52},
+    //   {group: "B", variable: "v6", value: 30},
+    //   {group: "B", variable: "v7", value: 20},
+    //   {group: "B", variable: "v8", value: 99},
+    //   {group: "B", variable: "v9", value: 52},
+    //   {group: "B", variable: "v10", value: 30},
+    //   {group: "C", variable: "v1", value: 30},
+    //   {group: "C", variable: "v2", value: 95},
+    //   {group: "C", variable: "v3", value: 22},
+    //   {group: "C", variable: "v4", value: 14},
+    //   {group: "C", variable: "v5", value: 52},
+    //   {group: "C", variable: "v6", value: 30},
+    //   {group: "C", variable: "v7", value: 20},
+    //   {group: "C", variable: "v8", value: 99},
+    //   {group: "C", variable: "v9", value: 52},
+    //   {group: "C", variable: "v10", value: 30},
+    // ];
+
+    heatMap.svg.selectAll('rect')
+        .data(data, d => d.ostID)
+        .join('rect')
+        // .append("rect")
+          .attr("x", function(d) { return HTxAxis(String(d.xIndex)) })
+          .attr("y", function(d) { return HTyAxis(String(d.yIndex)) })
+          .attr("width", HTxAxis.bandwidth() )
+          .attr("height", HTyAxis.bandwidth() )
+          .style("fill", function(d) { return myColor(d.size)} )
+
+  });
+}
